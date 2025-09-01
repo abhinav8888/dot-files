1e009dd67caa3bfc9cdc1300806d8d895fcf0853
abhinav <abhinav9011032@gmail.com>
2025-08-31 22:54:41 -0700

    Change theme to amro

--- zsh/.zsh/evals.zsh
+++ zsh/.zsh/evals.zsh
@@ -1,3 +1,3 @@
 eval "$(zoxide init zsh)"
 source <(fzf --zsh)
-eval "$(oh-my-posh init zsh --config '~/.config/themes/m365.yaml')"
+eval "$(oh-my-posh init zsh --config '~/.config/themes/amro.yaml')"
