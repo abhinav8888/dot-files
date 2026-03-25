c2be0ef0ca798895ac3973dbc6b2dc1641a2c4e3
Kumar Abhinav <abhinav9011032@gmail.com>
2026-03-25 00:56:41 -0700

    Entire enabled

--- zsh/.zsh/evals.zsh
+++ zsh/.zsh/evals.zsh
@@ -1,7 +1,7 @@
 eval "$(zoxide init zsh)"
 source <(fzf --zsh)
 # Terminals that support oh-my-posh prompt
-local _omp_terminals=(ghostty WezTerm alacritty)
+local _omp_terminals=(ghostty WezTerm alacritty iTerm.app)
 if (( ${_omp_terminals[(Ie)$TERM_PROGRAM]} )); then
   eval "$(oh-my-posh init zsh --config '~/.config/themes/amro.yaml')"
 fi
